511e0345f6a60e21d48e16771043e379e613f1a6
calvincho95 <calvincho95@naver.com>
2024-05-27 20:52:40 +0900

    speak-main-page

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,4 +1,4 @@
-import image from "../assets/images/657a79fcf1b0fa6087a250e4_new-phone-scrim.webp";
+import image from "../assets/images/phone.png";
 
 const MainScreen = () => {
   return (
@@ -12,25 +12,27 @@ const MainScreen = () => {
     // </main>
     <main className="container mx-auto flex flex-colu md:flex-row items-center  p-8">
       {/* jtem1 */}
-      <div className="flex-1 bg-white p-6 rounded-md">
-        <h1 className="text-4xl font-bold mb-4">
-          당신을 실제로 말하게 하는 영어 앱
+      <div className="flex-1 text-left ml-0 p-6 rounded-md">
+        <h1 className="text-5xl font-bold mb-4">
+          당신을 실제로 <br />
+          말하게 하는 영어 앱
         </h1>
-        <p className="text-lg mb-4">
-          사람보다 나은 AI 영어 선생님과 마음껏 대화하고, 실시간 피드백을
-          받아보세요. 금방 유창한 영어 실력을 갖게 될 거예요.
+        <p className="text-sm mb-4 text-[#8990AD]">
+          사람보다 나은 AI 영어 선생님과 <br />
+          마음껏 대화하고, 실시간 피드백을 받아보세요. <br />
+          금방 유창한 영어 실력을 갖게 될 거예요.
         </p>
         <a
           href="#"
-          className="bg-blue-600 text-white px-6 py-3 rounded-full  inline-block"
+          className=" bg-[#1C49FF] text-white px-5 hover:bg-[#1c49ffaf] transition-colors duration-1000 py-4 rounded-full  inline-block"
         >
           무료 체험 시작하기
         </a>
       </div>
 
       {/* item2 */}
-      <div className="flex-1 mt-8 md:mt-0 md:ml-8 bg-white p-6 rounded-md ">
-        <img src={image} alt="Phone App" className="w-full h-auto" />
+      <div className="flex-1 mt-8 md:mt-0 md:ml-8 p-6 rounded-md ">
+        <img src={image} alt="Phone App" className="max-w-[480px] w-full" />
       </div>
     </main>
   );
--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -1,4 +1,5 @@
 import image from "../assets/images/logo.svg";
+import face from "../assets/images/face.png";
 
 const TopBanner = () => {
   return (
@@ -17,33 +18,28 @@ const TopBanner = () => {
     //   </div>
     // </header>
     <div className=" text-white">
-      <div className="container mx-auto flex justify-between items-center py-4">
+      <div className="mx-auto flex justify-between items-center px-4 py-4 mt-4  rounded-full shadow-xl mx-8">
         <div className="flex items-center">
-          <img src={image} alt="Speak" className="w=[110px] h=[26.87px]" />
+          <img src={image} alt="Speak" className="w-[110px] h-[26.87px]" />
         </div>
-        <div className="flex space-x-4">
-          <button className="text-black h=[100px] w=[46px]">
-            천하제일 변명대회
+        <div className=" flex items-center space-x-4">
+          <button className="text-black border rounded-full font-bold text-sm hover:text-[#1C49FF] transition-colors duration-1000 px-8 py-3 h=[100px] w=[46px] flex items-center">
+            <img src={face} alt="face" className="w-[25px] h-[20px]" /> 천하제일
+            변명대회
           </button>
-          <button className="text-black h=[95.22px] w=[20px]">
+          <button className="text-black hover:bg-[#cdcdcd3f] transition-colors duration-1000 hover:text-[#1C49FF] text-sm rounded-full px-4 py-3">
             스픽: 더 알아보기
           </button>
-          <button className="text-black h=[77.45px] w=[20px]">
+          <button className="text-black hover:bg-[#cdcdcd3f] transition-colors duration-1000 hover:text-[#1C49FF] text-sm rounded-full px-4 py-3">
             비즈니스/제휴
           </button>
-          <button
-            style={{ backgroundColor: "#F5F5F5", fontSize: "14px" }}
-            className="text-black border rounded-full px-8 py-2  "
-          >
+          <button className="text-black border rounded-full bg-[#F5F5F5]  hover:bg-[#3f5edc2f] transition-colors duration-1000 px-9 py-3 pg-[#F5F5F5] text-sm ">
             🇰🇷 한국어
           </button>
+          <button className="text-white px-4 py-3 rounded-full bg-[#1C49FF] hover:bg-[#1c49ffaf] transition-colors duration-700 text-sm">
+            무료 체험 시작하기 →
+          </button>
         </div>
-        <button
-          style={{ backgroundColor: "#1C49FF", fontSize: "14px" }}
-          className="text-white px-10 py-2 rounded-full "
-        >
-          무료 체험 시작하기
-        </button>
       </div>
     </div>
   );
